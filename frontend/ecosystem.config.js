5b8f046ab0f1aff1758127382745339688bc40e0
psolfgux <psolf@gux.tech>
2025-03-05 11:37:18 -0300

    actuzliacion

--- frontend/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = [{
-  script: 'server.js',
-  name: 'Whazing-frontend',
-  exec_mode: 'fork',
-  cron_restart: '00 00 * * *',
-  instances: 1,
-  watch: false
-}]
--- frontend/src/service/protocolos.js
+++
@@ -1,9 +0,0 @@
-import request from 'src/service/request'
-
-export function ListarProtocolos(params) {
-  return request({
-    url: '/protocol',
-    method: 'get',
-    params
-  })
-}
